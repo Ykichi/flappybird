e0ba16a3922d21427ac19635d4764a0dd74bac21
YKichi <acheronkichi@gmail.com>
2018-07-05 17:53:17 +0900

    first commit

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+	
+	//Movement speed
+	public float speed = 2;
+
+	//Flap force
+	public float force = 300;
+
+	// Use this for initialization
+	void Start () {
+		//Fly towards the right
+		GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			//Flap
+			GetComponent<Rigidbody2D>().AddForce(Vector2.up * force);
+		}
+	}
+
+	/// <summary>
+	/// Sent when an incoming collider makes contact with this object's
+	/// collider (2D physics only).
+	/// </summary>
+	/// <param name="other">The Collision2D data associated with this collision.</param>
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		//Restart
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+	//Movement speed (0 means don't move)
+	public float speed = 0;
+	//Swich movement direction every few seconds
+	public float switchTime = 2;
+
+	// Use this for initialization
+	void Start () {
+		//Initial movement direction
+		GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+		//Switch every few seconds 
+		InvokeRepeating("obstacleSwitch", 0, switchTime);
+	}
+
+	void obstacleSwitch () {
+		GetComponent<Rigidbody2D>().velocity *= -1;
+	}
+	
+}
